8b9d3300342322a90d580b2bbdccfb1f9be04b25
Nov11 <529079634@qq.com>
2017-09-18 06:50:56 +0800

    2017/9/18

--- k Sum .cpp
+++ k Sum .cpp
@@ -16,6 +16,31 @@ public:
 
 		int len = A.size();
 
+		vector<vector<int>> v(k + 1, vector<int>(target + 1));
+		v[0][0] = 1;
+		for (int i = 1; i <= len; i++) {
+			for (int j = k; j >= 1; j--) {
+				for (int t = target; t >= 1; t--) {
+					//cur state : i, j, t
+					if (t - A[i - 1] >= 0) {
+						v[j][t] += v[j - 1][t - A[i - 1]];
+					}
+				}
+			}
+		}
+
+		return v[k][target];
+	}
+	int kSumOld(vector<int> &A, int k, int target) {
+		// write your code here
+		if (k == 0 && target == 0) { return 1; }
+		if (k <= 0) { return 0; }
+		if (target <= 0) { return 0; }
+
+		if (A.size() < k) { return 0; }
+
+		int len = A.size();
+
 		vector<vector<vector<int>>> v(len + 1, vector<vector<int>>(k + 1, vector<int>(1 + target)));
 		v[0][0][0] = 1;
 		for (int t = 1; t <= target; t++) {
