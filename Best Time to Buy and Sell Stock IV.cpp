5b2d981fef79d2c3fb3d79e385c7c6817a67c56f
Nov11 <529079634@qq.com>
2017-12-28 17:13:36 +0800

    2017/12/28

---
+++ Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+	/*
+	* @param K: An integer
+	* @param prices: An integer array
+	* @return: Maximum profit
+	*/
+	int maxProfit(int K, vector<int> &prices) {
+		// write your code here
+		if (prices.size() <= 1) {
+			return 0;
+		}
+		if (K >= prices.size() / 2) {
+			int result = 0;
+			for (int i = 1; i < prices.size(); i++) {
+				if (prices[i] > prices[i - 1]) {
+					result += prices[i] - prices[i - 1];
+				}
+			}
+			return result;
+		}
+
+		int len = prices.size();
+		vector<int> global(len);
+		vector<int> g(len);
+		for (int k = 1; k <= K; k++) {
+
+			int l = 0;
+			for (int i = 1; i < len; i++) {
+
+				int diff = prices[i] - prices[i - 1];
+				l = max(l + diff, max(global[i - 1] + diff, global[i - 1]));
+				g[i] = max(g[i - 1], l);
+			}
+			swap(g, global);
+		}
+
+		return global[len - 1];
+	}
+};
